3d5f4098776623a8690bc7ed371c21219e790716
samarth240910-svg <samarth240910@gmail.com>
2026-02-08 16:38:24 +0530

    Update LifestealPlugin.java

--- src/main/java/lifesteal/LifestealPlugin.java
+++ src/main/java/lifesteal/LifestealPlugin.java
@@ -1,1 +1,37 @@
+package lifesteal;
+
+import lifesteal.combat.CombatTagManager;
+import lifesteal.heart.HeartManager;
+import lifesteal.listeners.DeathListener;
+import lifesteal.listeners.JoinListener;
+import lifesteal.listeners.LootListener;
+import lifesteal.commands.HeartCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LifestealPlugin extends JavaPlugin {
+
+    private HeartManager heartManager;
+    private CombatTagManager combatTagManager;
+
+    @Override
+    public void onEnable() {
+        heartManager = new HeartManager(this);
+        combatTagManager = new CombatTagManager();
+
+        Bukkit.getPluginManager().registerEvents(
+                new DeathListener(heartManager, combatTagManager), this);
+        Bukkit.getPluginManager().registerEvents(
+                new LootListener(), this);
+        Bukkit.getPluginManager().registerEvents(
+                new JoinListener(heartManager), this);
+
+        getCommand("hearts").setExecutor(
+                new HeartCommand(heartManager));
+
+        heartManager.registerRecipe();
+
+        getLogger().info("Lifesteal enabled successfully.");
+    }
+}
 
