bc18c905c0bba0f95eab6950eec53f36ada8df34
samarth240910-svg <samarth240910@gmail.com>
2026-02-08 16:32:39 +0530

    Update LootListener.java

--- src/main/java/lifesteal/listeners/LootListener.java
+++ src/main/java/lifesteal/listeners/LootListener.java
@@ -1,1 +1,15 @@
+package lifesteal.listeners;
+
+import lifesteal.heart.HeartItem;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.LootGenerateEvent;
+
+public class LootListener implements Listener {
+
+    @EventHandler
+    public void onLoot(LootGenerateEvent event) {
+        event.getLoot().removeIf(HeartItem::isHeart);
+    }
+}
 
