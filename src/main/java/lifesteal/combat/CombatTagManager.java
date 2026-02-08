47d751ad1db8f5637e61d969f3984b00e95edc0f
samarth240910-svg <samarth240910@gmail.com>
2026-02-08 16:37:05 +0530

    Update CombatTagManager.java

--- src/main/java/lifesteal/combat/CombatTagManager.java
+++ src/main/java/lifesteal/combat/CombatTagManager.java
@@ -1,1 +1,25 @@
+package lifesteal.combat;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class CombatTagManager {
+
+    private final Set<UUID> combatTagged = new HashSet<>();
+
+    public void tag(Player player) {
+        combatTagged.add(player.getUniqueId());
+    }
+
+    public void untag(Player player) {
+        combatTagged.remove(player.getUniqueId());
+    }
+
+    public boolean isTagged(Player player) {
+        return combatTagged.contains(player.getUniqueId());
+    }
+}
 
